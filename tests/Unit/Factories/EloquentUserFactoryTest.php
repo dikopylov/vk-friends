08735bdb7cca9a05b2d15d3b9ef5fcfc2e54c240
Dmitry Kopylov <dikopylov10@gmail.com>
2022-01-19 08:33:58 +0500

    Added unit test for factory

--- tests/Unit/Factories/EloquentUserFactoryTest.php
+++ tests/Unit/Factories/EloquentUserFactoryTest.php
@@ -3,36 +3,125 @@
 namespace Tests\Unit\Factories;
 
 use App\Factories\EloquentUserFactory;
-use App\Models\User;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Tests\Unit\Stubs\SocialiteUserStub;
+use Tests\Unit\Stubs\UserStub;
 use Tests\Unit\UnitTestCase;
 
 class EloquentUserFactoryTest extends UnitTestCase
 {
 
-    public function testMake_hasNotUser_returnUser()
+    public function testMake_hasNotUser_returnNewUser()
     {
-        $auth = $this->prophesize(Model::class);
+        $userRepository = $this->prophesize(UserStub::class);
+        $user           = $this->prophesize(UserStub::class);
+
+        $id       = $this->faker->randomDigitNotNull();
+        $nickname = $this->faker->userName();
+        $name     = $this->faker->name();
+        $email    = $this->faker->email();
+        $avatar   = $this->faker->imageUrl();
+        $password = $this->faker->password();
 
         $socialiteUser = new SocialiteUserStub(
-            $this->faker->randomDigitNotNull(),
-            $this->faker->userName(),
-            $this->faker->name(),
-            $this->faker->email(),
-            $this->faker->imageUrl(),
+            $id,
+            $nickname,
+            $name,
+            $email,
+            $avatar,
         );
 
-        $actual = (new EloquentUserFactory($auth->reveal()))->make($socialiteUser);
 
-        $expected = new User();
-        $expected->id       = $socialiteUser->getId();
-        $expected->name     = $socialiteUser->getName();
-        $expected->email    = $socialiteUser->getEmail();
-        $expected->avatar   = $socialiteUser->getAvatar();
-        $expected->nickname = $socialiteUser->getNickname();
-        $expected->password = false;
+        Hash::partialMock()->expects('make')->andReturns($password);
+
+        $user->setAttribute('id', $id)->shouldBeCalled();
+        $user->setAttribute('nickname', $nickname)->shouldBeCalled();
+        $user->setAttribute('name', $name)->shouldBeCalled();
+        $user->setAttribute('email', $email)->shouldBeCalled();
+        $user->setAttribute('avatar', $avatar)->shouldBeCalled();
+        $user->exists = false;
+        $user->setAttribute('password', $password)->shouldBeCalled();
+        $user->isDirty()->shouldBeCalled()->willReturn(true);
+        $user->save()->shouldBeCalled()->willReturn(true);
+
+        $userRepository->findOrNew($id)->shouldBeCalled()->willReturn($user->reveal());
+
+        (new EloquentUserFactory($userRepository->reveal()))->make($socialiteUser);
+    }
+
+    public function testMake_hasUser_returnExistsUser()
+    {
+        $userRepository = $this->prophesize(UserStub::class);
+        $user           = $this->prophesize(UserStub::class);
+
+        $id       = $this->faker->randomDigitNotNull();
+        $nickname = $this->faker->userName();
+        $name     = $this->faker->name();
+        $email    = $this->faker->email();
+        $avatar   = $this->faker->imageUrl();
+        $password = $this->faker->password();
+
+        $socialiteUser = new SocialiteUserStub(
+            $id,
+            $nickname,
+            $name,
+            $email,
+            $avatar,
+        );
+
+
+        Hash::partialMock()->expects('make')->never();
+
+        $user->setAttribute('id', $id)->shouldBeCalled();
+        $user->setAttribute('nickname', $nickname)->shouldBeCalled();
+        $user->setAttribute('name', $name)->shouldBeCalled();
+        $user->setAttribute('email', $email)->shouldBeCalled();
+        $user->setAttribute('avatar', $avatar)->shouldBeCalled();
+        $user->exists = true;
+        $user->setAttribute('password', $password)->shouldNotBeCalled();
+        $user->isDirty()->shouldBeCalled()->willReturn(false);
+        $user->save()->shouldNotBeCalled();
+
+        $userRepository->findOrNew($id)->shouldBeCalled()->willReturn($user->reveal());
+
+        (new EloquentUserFactory($userRepository->reveal()))->make($socialiteUser);
+    }
+
+    public function testMake_hasModifiedUser_returnExistsUser()
+    {
+        $userRepository = $this->prophesize(UserStub::class);
+        $user           = $this->prophesize(UserStub::class);
+
+        $id       = $this->faker->randomDigitNotNull();
+        $nickname = $this->faker->userName();
+        $name     = $this->faker->name();
+        $email    = $this->faker->email();
+        $avatar   = $this->faker->imageUrl();
+        $password = $this->faker->password();
+
+        $socialiteUser = new SocialiteUserStub(
+            $id,
+            $nickname,
+            $name,
+            $email,
+            $avatar,
+        );
+
+
+        Hash::partialMock()->expects('make')->never();
+
+        $user->setAttribute('id', $id)->shouldBeCalled();
+        $user->setAttribute('nickname', $nickname)->shouldBeCalled();
+        $user->setAttribute('name', $name)->shouldBeCalled();
+        $user->setAttribute('email', $email)->shouldBeCalled();
+        $user->setAttribute('avatar', $avatar)->shouldBeCalled();
+        $user->exists = true;
+        $user->setAttribute('password', $password)->shouldNotBeCalled();
+        $user->isDirty()->shouldBeCalled()->willReturn(true);
+        $user->save()->shouldBeCalled()->willReturn(true);
+
+        $userRepository->findOrNew($id)->shouldBeCalled()->willReturn($user->reveal());
+
+        (new EloquentUserFactory($userRepository->reveal()))->make($socialiteUser);
     }
 }
---
+++ tests/Unit/Stubs/UserStub.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Tests\Unit\Stubs;
+
+
+/**
+ *
+ * @method self|null findOrNew($id)
+ */
+class UserStub extends \Illuminate\Foundation\Auth\User
+{
+
+}
